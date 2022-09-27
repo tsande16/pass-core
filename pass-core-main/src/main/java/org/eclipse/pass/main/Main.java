434cc69ee15795d53228f2db6138226a14840c01
Mark Patton <mark.patton@gmail.com>
2022-09-27 10:32:04 -0400

    Set copyright statements and fix checkstyle problems

--- pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
+++ pass-core-main/src/main/java/org/eclipse/pass/main/Main.java
@@ -1,21 +1,31 @@
 /*
- * Copyright 2019, Yahoo Inc.
- * Licensed under the Apache License, Version 2.0
- * See LICENSE file in project root for terms.
+ * Copyright 2022 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.eclipse.pass.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ServletComponentScan(basePackages = { "org.dataconservancy.pass.doi.service" })
-@ComponentScan(basePackages = {"org.eclipse.pass.main", "org.dataconservancy.pass.doi.service" })
+@ComponentScan(basePackages = {"org.eclipse.pass.main"})
 @EntityScan(basePackages = { "org.eclipse.pass.object.model" })
 public class Main {
+    protected Main() {}
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Main.class, args);
     }
