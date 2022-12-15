996c083453b701fee2d088b1648ca274e4092e18
Timothy Sanders <tsande16@jhu.edu>
2022-12-15 14:34:34 -0500

    File UPLOAD/GET/DELETE endpoint working in docker

--- pass-core-file-service/src/main/java/org/eclipse/pass/file/service/PassFileServiceController.java
+++ pass-core-file-service/src/main/java/org/eclipse/pass/file/service/PassFileServiceController.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2019 Johns Hopkins University
+ * Copyright 2023 Johns Hopkins University
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -16,69 +16,80 @@
  */
 package org.eclipse.pass.file.service;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Paths;
 
-import org.eclipse.pass.file.service.storage.StorageConfiguration;
+import org.eclipse.pass.file.service.storage.FileStorageService;
 import org.eclipse.pass.file.service.storage.StorageFile;
-import org.eclipse.pass.file.service.storage.StorageService;
-import org.eclipse.pass.file.service.storage.StorageServiceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
-
 /**
  * PassFileServiceController is the controller class responsible for the File Service endpoints, which allows pass-core
- * internal and external services to upload, retrieve and delete files.
+ * internal and external services to upload, retrieve and delete files. Configuration of the File Service is done
+ * through .env file and is loaded into the StorageProperties.
  *
- * Configuration of the File Service is done through .env environment variable file.
+ * @author Tim Sanders
  */
 @RestController
 public class PassFileServiceController {
     private static final Logger LOG = LoggerFactory.getLogger(PassFileServiceController.class);
-    private StorageService storageService;
 
+    @Lazy
     @Autowired
-    StorageConfiguration storageConfiguration;
+    private FileStorageService fileStorageService;
 
     /**
      *   Class constructor.
      */
-    public void PassFileServiceController() {
+    public PassFileServiceController() {
     }
 
     /**
-     * Handles a file upload and will call the StorageService to determine the repository where the file is to be
+     * Handles a file upload and will call the FileStorageService to determine the repository where the file is to be
      * deposited.
      *
-     * @param fileName
+     * @param file A multipart file that is uploaded from the client.
      * @return return a File object that has been uploaded.
-     * @throws FileServiceException If fail to call the API, e.g. server error or cannot deserialize the response body
      */
     @PostMapping("/file")
-    public ResponseEntity<StorageFile> fileUpload(@RequestParam("file") MultipartFile file,
-                                                  @RequestParam("fileName") String fileName)
-            throws FileServiceException {
-        LOG.info("Uploading New File");
-        StorageServiceFactory storageFactory = new StorageServiceFactory();
-        storageService = storageFactory.createStorage(storageConfiguration.getStorageProperties());
-        if (fileName == null) {
-            throw new FileServiceException(HttpStatus.NOT_FOUND, "Missing File Name");
+    public ResponseEntity<?> fileUpload(@RequestParam("file") MultipartFile file) {
+        StorageFile returnStorageFile;
+
+        try {
+            if (file.getBytes().length == 0) {
+                return ResponseEntity.badRequest().build();
+            }
+        } catch (IOException e) {
+            LOG.error("File Service: Error processing file upload: " + e);
+            return ResponseEntity.notFound().build();
         }
-        LOG.info("Storing File");
-        StorageFile returnStorageFile = storageService.store(file, fileName);
-        LOG.info("Storing File");
+
+        try {
+            returnStorageFile = fileStorageService.storeFile(file);
+        } catch (IOException e) {
+            LOG.error("File Service: Error storing file upload: " + e);
+            return ResponseEntity.notFound().build();
+        } catch (NullPointerException e) {
+            LOG.error("File Service: Error storing file upload: " + e);
+            return ResponseEntity.notFound().build();
+        }
+
         return ResponseEntity.created(URI.create(returnStorageFile.getId())).body(returnStorageFile);
     }
 
@@ -86,20 +97,39 @@ public class PassFileServiceController {
      * Gets a file by the fileId and returns a single file. Implicitly supports HTTP HEAD.
      *
      * @param fileId ID of the file to return (required)
-     * @return Bitstream
-     * @throws FileServiceException If fail to call the API, e.g. server error or cannot deserialize the response body
+     * @return Bitstream The file requested by the fileId
      */
-    @GetMapping("/file/{fileId}")
-    public ResponseEntity<String> getFileById(@PathVariable String fileId) throws FileServiceException {
-        System.out.println("Get file by ID");
-        LOG.info("Get file by ID.");
+    @GetMapping("/file/{fileId:.+}")
+    @ResponseBody
+    public ResponseEntity<?> getFileById(@PathVariable String fileId) {
+        LOG.info("Get file by ID. File ID: " + fileId);
         if (fileId == null) {
             LOG.error("File ID not provided to get a file.");
-            throw new FileServiceException(HttpStatus.NOT_FOUND, "Missing File ID");
+            return ResponseEntity.badRequest().body("File ID not provided to get a file.");
         }
-        //TODO implement
-        //testing
-        return ResponseEntity.ok().body("All good.");
+        ByteArrayResource fileResource;
+        String fileName = "";
+        try {
+            fileName = Paths.get(fileStorageService.getResourceFileRelativePath(fileId)).getFileName().toString();
+        } catch (Exception e) {
+            LOG.info("Get file by ID. File ID not found: " + fileId);
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            fileResource = fileStorageService.getFile(fileId);
+        } catch (Exception e) {
+            LOG.error("File Service: File not found: " + e);
+            return ResponseEntity.notFound().build();
+        }
+
+        LOG.info("File Service: Filename= " + fileName);
+        String headerAttachment = "attachment; filename=\"" + fileName + "\"";
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerAttachment)
+                .contentLength(fileResource.contentLength())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(fileResource);
     }
 
     /**
@@ -107,16 +137,15 @@ public class PassFileServiceController {
      *
      * @param fileId ID of the file to delete (required)
      * @return File
-     * @throws FileServiceException If fail to call the API, e.g. server error or cannot deserialize the response body
      */
     @DeleteMapping("/file/{fileId}")
-    public ResponseEntity<Resource> deleteFileById(@PathVariable String fileId) throws FileServiceException {
-        LOG.info("Get file by ID.");
+    public ResponseEntity<?> deleteFileById(@PathVariable String fileId) {
+        LOG.info("Delete file by ID.");
         if (fileId == null) {
             LOG.error("File ID not provided to delete file.");
-            throw new FileServiceException(HttpStatus.NOT_FOUND, "Missing File ID");
+            return ResponseEntity.notFound().build();
         }
-        //TODO implement
-        return null;
+        fileStorageService.deleteFile(fileId);
+        return ResponseEntity.ok().body("Deleted");
     }
 }
--- pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageFile.java
+++ pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageFile.java
@@ -1,37 +1,63 @@
+/*
+ *
+ * Copyright 2023 Johns Hopkins University
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package org.eclipse.pass.file.service.storage;
 
 import java.util.Objects;
 
-import com.google.gson.annotations.SerializedName;
-
+/**
+ * A FileService StorageFile is a representation of a file (bytes) that is persisted into storage or repository.
+ *
+ * @author Tim Sanders
+ */
 public class StorageFile {
-    @SerializedName("id")
-    private String id = null;
+    private String id;
+    private String fileName;
+    private String mimeType;
+    private String storageType;
+    private Long size;
+    private String extension;
 
-    @SerializedName("fileName")
-    private String fileName = null;
-
-    @SerializedName("mimeType")
-    private String mimeType = null;
-
-    @SerializedName("storageType")
-    private String storageType = null;
-
-    @SerializedName("size")
-    private Long size = null;
-
-    @SerializedName("extension")
-    private String extension = null;
-
+    /**
+     * Default Constructor which initializes the StorageFile with default value for member variables
+     *
+     */
     public StorageFile() {
         this.id = "999";
-        this.id = "DefaultFileName";
+        this.fileName = "DefaultFileName";
         this.mimeType = "DefaultMineType";
         this.storageType = "DefaultStorageType";
         this.size = 0L;
         this.extension = "DefaultExtension";
     }
 
+    /**
+     * Initializes the StorageFile with the supplied arguments.
+     *
+     * @param id The identifier of the StorageFile.
+     * @param fileName The name of the file that is loaded into the repository.
+     * @param mimeType The mime tye of the file that is loaded into the repository.
+     * @param storageType The storage type associated with the file. If a file was stored into a file system, this
+     *                    value would be FILE_SYSTEM. See the StorageServiceUtils class for the enum that has all the
+     *                    valid storageType values.
+     * @param size The size of the file that is loaded into the repository
+     * @param extension The extension of the file that is loaded into the repository
+     *
+     * @see StorageServiceUtils
+     */
     public StorageFile(String id, String fileName, String mimeType, String storageType, Long size, String extension) {
         this.id = id;
         this.fileName = fileName;
@@ -43,72 +69,96 @@ public class StorageFile {
 
     /**
      * Get id
-     * @return id
+     * @return id of the StorageFile
      **/
     public String getId() {
         return id;
     }
 
+    /**
+     * Set id
+     * @param id of the StorageFile
+     **/
     public void setId(String id) {
         this.id = id;
     }
 
     /**
      * Get fileName
-     * @return fileName
+     * @return fileName of the StorageFile
      **/
     public String getFileName() {
         return fileName;
     }
 
+    /**
+     * Set fileName
+     * @param fileName of the StorageFile
+     **/
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
 
     /**
      * Get mimeType
-     * @return mimeType
+     * @return mimeType of the StorageFile
      **/
     public String getMimeType() {
         return mimeType;
     }
 
+    /**
+     * Set mimeType
+     * @param mimeType of the StorageFile
+     **/
     public void setMimeType(String mimeType) {
         this.mimeType = mimeType;
     }
 
     /**
      * Get storageType
-     * @return storageType
+     * @return storageType of the StorageFile
      **/
     public String getStorageType() {
         return storageType;
     }
 
+    /**
+     * Set storageType
+     * @param storageType of the StorageFile
+     **/
     public void setStorageType(String storageType) {
         this.storageType = storageType;
     }
 
     /**
      * Get size
-     * @return size
+     * @return size of the StorageFile
      **/
     public Long getSize() {
         return size;
     }
 
+    /**
+     * Get size
+     * @param size of the StorageFile
+     **/
     public void setSize(Long size) {
         this.size = size;
     }
 
     /**
      * Get extension
-     * @return extension
+     * @return extension of the StorageFile
      **/
     public String getExtension() {
         return extension;
     }
 
+    /**
+     * Set extension
+     * @param extension of the StorageFile
+     **/
     public void setExtension(String extension) {
         this.extension = extension;
     }
