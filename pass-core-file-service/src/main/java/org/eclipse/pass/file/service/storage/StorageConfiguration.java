f98b4dbc94368a826a5a33422d99bb7aa64d209c
Timothy Sanders <tsande16@jhu.edu>
2022-12-15 14:34:34 -0500

    File UPLOAD/GET/DELETE endpoint working in docker

--- pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageConfiguration.java
+++ pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageConfiguration.java
@@ -1,24 +1,66 @@
+/*
+ *
+ * Copyright 2023 Johns Hopkins University
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package org.eclipse.pass.file.service.storage;
 
-import org.eclipse.pass.file.service.PassFileServiceController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The StorageConfiguration is responsible for handling the StorageProperties. The FileStorageService does not get the
+ * storage configuration directly but through the StorageConfiguration.
+ *
+ * @author Tim Sanders
+ * @see StorageProperties
+ */
 @Configuration
 @EnableConfigurationProperties(StorageProperties.class)
 public class StorageConfiguration {
-    private static final Logger LOG = LoggerFactory.getLogger(PassFileServiceController.class);
-
     private StorageProperties storageProperties;
 
+    private StorageConfiguration() {
+    }
+
+    /**
+     * StorageConfiguration constructor that initializes the StorageProperties.
+     *
+     * @param storageProperties are injected into the StorageConfiguration at startup.
+     */
     public StorageConfiguration(StorageProperties storageProperties) {
         this.storageProperties = storageProperties;
     }
 
+    /**
+     * Gets the StorageProperties of the StorageConfiguration.
+     *
+     * @return An instance of the StorageProperties
+     */
     public StorageProperties getStorageProperties() {
         return storageProperties;
+    }
+
+    /**
+     * Sets the StorageProperties of the StorageConfiguration. The FileService is designed to load the application
+     * variables defined in the .env file. To override the StorageProperties should be used with caution as there might
+     * be unintended results. If the configuration of the FileService needs to be modified, do so in the .env file.
+     *
+     * @param storageProperties The StorageProperties to set in the StorageConfiguration.
+     */
+    public void setStorageProperties(StorageProperties storageProperties) {
+        this.storageProperties = storageProperties;
     }
 
 }
--- pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageProperties.java
+++ pass-core-file-service/src/main/java/org/eclipse/pass/file/service/storage/StorageProperties.java
@@ -1,7 +1,31 @@
+/*
+ *
+ * Copyright 2023 Johns Hopkins University
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package org.eclipse.pass.file.service.storage;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * StorageProperties are the File Service configuration properties indicating where and what type of storage is used
+ * for persistence. The StorageProperties are managed by the StorageConfiguration.
+ * Important: The OCFL root and working directory must be on the same mount.
+ *
+ * @author Tim Sanders
+ * @see StorageConfiguration
+ */
 @ConfigurationProperties(prefix = "spring.file-service")
 public class StorageProperties {
     private String ocflRootDir;
@@ -9,18 +33,36 @@ public class StorageProperties {
     private String tempDir;
     private String storageType;
 
+    /**
+     * Get the logical absolute path of the storage root directory
+     * @return The logical path of the OCFL storage root
+     */
     public String getStorageRootDir() {
         return ocflRootDir;
     }
 
+    /**
+     * Get the logical absolute path of the OCFL working directory. This directory must be on the same
+     * mount as the OCFL root directory
+     * @return The logical path of the OCFL working directory
+     */
     public String getStorageWorkDir() {
         return ocflWorkDir;
     }
 
+    /**
+     * Get the logical path of the temp directory. This directory is not the absolute path, but the relative
+     * path of the temp directory. All temp files are stored within the {system_temp_directory}/tempDir
+     * @return The logical path of the temp directory
+     */
     public String getStorageTempDir() {
         return tempDir;
     }
 
+    /**
+     * Get the storage type that is configured for the File Service
+     * @return The storage type of the File Service
+     */
     public String getStorageType() {
         return storageType;
     }
